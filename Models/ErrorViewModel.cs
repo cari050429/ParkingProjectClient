2461ab361fc6a157ea23afab06fc0998dcad52d6
cari050429 <corrales91486@gmail.com>
2025-01-12 23:00:18 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkingProjectClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
